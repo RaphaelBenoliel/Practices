d29814b8983a6888fbfc7ed8487314060ffb570f
Raphael Benoliel <raphael2gb@gmail.com>
2022-11-24 21:13:33 +0200

    task 4

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 ################################################################################
 ################################ TASK__1 #######################################
 ################################################################################
-
+'''
 """
 Ques 1 - celsius to far
 """
@@ -263,7 +263,7 @@ def smooth(f):
 a = smooth(9)
 print(a)
 
-'''
+
 from turtle import *
 color('red', 'yellow')
 begin_fill()
@@ -273,6 +273,52 @@ for i in range(36):
 end_fill()
 done()
 '''
+
+
 ################################################################################
 ################################ Task__4 #######################################
 ################################################################################
+class Student:
+    def __init__(self, firstName, lastName):
+        self.first = firstName
+        self.last = lastName
+        self.g = []
+
+    def __add__(self, x):
+        s = Student(self.first, self.last)
+        s.g = list(self.g) + [x, ]
+        return s
+
+    def __str__(self):
+        return f'Student name: {self.first} {self.last}'
+
+    def __lt__(self, other):
+        if other.first < self.first:
+            if other.last < self.last:
+                return True
+        return False
+
+    def __le__(self, other):
+        return (self.first, self.last) <= (other.first, other.last)
+
+    def __eq__(self, other):
+        return (self.first, self.last) == (other.first, other.last)
+
+    def __ne__(self, other):
+        return (self.first, self.last) != (other.first, other.last)
+
+    def __ge__(self, other):
+        return (self.first, self.last) >= (other.first, other.last)
+
+    def __gt__(self, other):
+        return (self.first, self.last) > (other.first, other.last)
+
+
+
+
+v = Student('Raphael', 'Benoliel')
+r = Student('a', 'a')
+y = Student('a', 'a')
+
+print(v < r)
+print(v != y)
